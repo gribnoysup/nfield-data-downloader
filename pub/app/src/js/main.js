43978e10ce96cfeaae16662a485df57add91b129
Petushkov Sergey <petushkov.sergey@gmail.com>
2015-01-15 11:37:21 +0000

    Add 'focus-on' directive (put focus on elem on contidion)

--- pub/app/src/js/main.js
+++ pub/app/src/js/main.js
@@ -1,4 +1,4 @@
-var downloadManager = angular.module('downloadManager', ['ngAnimate', 'app-factories', 'app-controllers']);
+var downloadManager = angular.module('downloadManager', ['ngAnimate', 'app-factories', 'app-controllers', 'angularModalService']);
 
 downloadManager.directive('downloadOptions', function(){
     return {
@@ -16,66 +16,92 @@ downloadManager.directive('assignInterviewers', function(){
     };
 });
 
-downloadManager.directive('dropTarget', function(){
-    return function($scope, $element){
-        $element.on('drop', function(event){
-            event.stopPropagation();
-            event.preventDefault();
-            
-            $scope.$apply(function(){
-                $scope.interviewersData = undefined;
-            });
-            
-            // dataTransfer check
-            event.dataTransfer = event.dataTransfer || event.originalEvent.dataTransfer;
-            
-            var file = event.dataTransfer.files[0],
-                readFile,
-                reader = new FileReader();
-            
-            reader.onload = function(e) {
-                var data = e.target.result;
-                switch (file.type) {
-                    case 'application/vnd.ms-excel':
-                        readFile = XLS.read(data, {type: 'binary'});
-                        break;
-                    case 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
-                        readFile = XLSX.read(data, {type: 'binary'});
-                        break;
-                    case 'text/plain':
-                        readFile = data;
-                        break;
-                    default:
-                        console.log(file);
-                        console.log('Unsupported file format, use .xls, .xlsx or .txt');
+downloadManager.directive('focusOn', ['$timeout', function($timeout){
+    return {
+        restrict: 'A',
+        scope: {
+            focusOn: '=focusOn'
+        },
+        link: function ($scope, $element) {
+            $scope.$watch('focusOn', function(newVal){
+                if (newVal === true) {
+                    $timeout(function(){
+                        $element[0].focus();
+                    });
                 }
+            });
+        }
+    };
+}]);
+
+downloadManager.directive('dropTarget', function(){
+    return {
+        link: function($scope, $element) {
+            
+            $scope.dropped = false;
+            
+            $element.on('drop', function(event){
+                
+                $scope.dropped = true;
+                
+                event.stopPropagation();
+                event.preventDefault();
                 
                 $scope.$apply(function(){
-                    $scope.interviewersData = readFile;
+                    $scope.interviewersData = undefined;
                 });
-            };
+                
+                // dataTransfer check
+                event.dataTransfer = event.dataTransfer || event.originalEvent.dataTransfer;
+                
+                var file = event.dataTransfer.files[0],
+                    readFile,
+                    reader = new FileReader();
+                
+                reader.onload = function(e) {
+                    var data = e.target.result;
+                    switch (file.type) {
+                        case 'application/vnd.ms-excel':
+                            readFile = XLS.read(data, {type: 'binary'});
+                            break;
+                        case 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
+                            readFile = XLSX.read(data, {type: 'binary'});
+                            break;
+                        case 'text/plain':
+                            readFile = data;
+                            break;
+                        default:
+                            console.log(file);
+                            console.log('Unsupported file format, use .xls, .xlsx or .txt');
+                    }
+                    
+                    $scope.$apply(function(){
+                        $scope.interviewersData = readFile;
+                    });
+                };
+                
+                reader.readAsBinaryString(file);
+                
+                $element.removeClass('dragged-over');
+                
+            });
             
-            reader.readAsBinaryString(file);
+            $element.on('dragenter', function(event){
+                event.stopPropagation();
+                event.preventDefault();
+                $element.addClass('dragged-over');
+            });
             
-            $element.removeClass('dragged-over');
+            $element.on('dragleave', function(event){
+                event.stopPropagation();
+                event.preventDefault();
+                $element.removeClass('dragged-over');
+            });
             
-        });
-        
-        $element.on('dragenter', function(event){
-            event.stopPropagation();
-            event.preventDefault();
-            $element.addClass('dragged-over');
-        });
-        
-        $element.on('dragleave', function(event){
-            event.stopPropagation();
-            event.preventDefault();
-            $element.removeClass('dragged-over');
-        });
-        
-        $element.on('dragover', function(event){
-            event.stopPropagation();
-            event.preventDefault();
-        });
+            $element.on('dragover', function(event){
+                event.stopPropagation();
+                event.preventDefault();
+            });
+        }
     };
 });
